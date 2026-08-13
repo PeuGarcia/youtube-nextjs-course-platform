c82d3106447bf0ac73349683a9ecbd4df38c06c0
Pedro Garcia <pedrogarcia4433@gmail.com>
2026-08-13 16:57:06 -0300

    feat: add navigation drawer

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,8 +1,12 @@
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "CodarSe - Todos os cursos"
+};
 
 export default function PageCursos() {
 
-
+    
     return (
         <main>Cursos</main>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CodarSe - Página inicial",
+};
 
 export default function PageHome() {
   return (
